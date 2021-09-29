2f07b7bfb734c16bcc0deb65ff7ed18febba3458
Jake Napper <napperjlg@gmail.com>
2021-09-29 19:10:06 +0100

    added node modules to git ignore

---
+++ src/gatsby-config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    siteMetadata: {
+        title: `xLively FPL`,
+        author: {
+            name: `Jake Napper`,
+            summary: `FPL addict creating a place to collect his thoughts`
+        },
+        description: `The musings of an FPL addict`,
+        siteUrl: 'https://localhost:8000',
+        social: {
+            twitter: `xlivelyfpl`
+        }
+    },
+    plugins: [
+        {
+            resolve: `gatsby-plugin-mdx`,
+            options: {
+                extensions: [`.md`, `.mdx`]
+            }
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/blog`,
+                name: `blog`
+            }
+        }
+    ]
+}
